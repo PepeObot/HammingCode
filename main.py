adfdfdbcf9f28a42fb946a0ad628fce24285048e
Alfonso German <germanalfonso788@gmail.com>
2026-04-15 12:34:16 -0300

    Agregue el menu principal

---
+++ main.py
@@ -0,0 +1,16 @@
+from PyQt6 import QtWidgets, uic
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("Ventanas/mainMenu.ui", self)
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
